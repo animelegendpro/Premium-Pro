315274fbb6dc3d3c5288d20563b4ae7241add0f1
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-16 14:26:07 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,27 +5,27 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give Me A Caption To Set.\n\nExample:- `/set_caption 📕 Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+       return await message.reply_text("**ɢɪᴠᴇ ᴍᴇ ᴀ ᴄᴀᴘᴛɪᴏɴ ᴛᴏ sᴇᴛ.\n\nExᴀᴍᴘʟᴇ:- `/set_caption 📕 Nᴀᴍᴇ ➠ : {filename} \n\n🔗 Sɪᴢᴇ ➠ : {filesize} \n\n⏰ Dᴜʀᴀᴛɪᴏɴ ➠ : {duration}`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption Successfully Added ✅**")
+    await message.reply_text("**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ sᴜᴄᴄᴇssғᴜʟʟʏ ᴀᴅᴅᴇᴅ ✓**")
 
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You Don't Have Any Custom Caption ❌**")
+        await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ✘**")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
+    await message.reply_text("**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ 🗑️**")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ:</b></u>\n\n`{caption}`")
     else:
-       await message.reply_text("**You Don't Have Any Custom Caption ❌**")
+       await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ✘**")
           
 
 
