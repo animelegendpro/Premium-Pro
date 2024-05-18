a5d83dd1eb01670191bafd8e76bc5c6c22e8b521
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-18 11:55:00 +0530

    Update myplan.py

--- plugins/myplan.py
+++ plugins/myplan.py
@@ -1,15 +1,19 @@
 import time
 from pyrogram import Client, filters
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup)
+from pyrogram.types import (
+    InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
 from helper.database import find_one, used_limit
 from helper.database import daily as daily_
 import datetime
-from datetime import datetime
+from datetime import timedelta, date, datetime
 from datetime import date as date_
 from helper.progress import humanbytes
 from helper.database import daily as daily_
 from helper.date import check_expi
 from helper.database import uploadlimit, usertype
+
+
+
 
 
 @Client.on_message(filters.private & filters.command(["myplan"]))
@@ -33,24 +37,15 @@ async def start(client, message):
     if ends:
         pre_check = check_expi(ends)
         if pre_check == False:
-            uploadlimit(message.from_user.id, 2147483652)
-            usertype(message.from_user.id, "Free")
+            uploadlimit(message.from_user.id, 1288490188)
+            usertype(message.from_user.id, "Fʀᴇᴇ")
     if ends == None:
-        text = f"**User ID :** `{message.from_user.id}` \n**Name :** {message.from_user.mention} \n\n**<b>🏷 ᴘʟᴀɴ :</b>** {user} \n\n<b>✓ ᴜᴘʟᴏᴀᴅ 2GB ғɪʟᴇs \n<b>✓ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ <b>: {humanbytes(limit)} \n<b>✓ ᴛᴏᴅᴀʏ ᴜsᴇᴅ </b>: {humanbytes(used)} \n</b>✓ ʀᴇᴍᴀɪɴ :</b> {humanbytes(remain)} \n</b>✓ ᴛɪᴍᴇᴏᴜᴛ :</b> 2 ᴍɪɴᴜᴛᴇs \n<b>✓ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇss :</b> ᴜɴʟɪᴍɪᴛᴇᴅ \n<b>✓ ᴛɪᴍᴇ ɢᴀᴘ :</b> ʏᴇs \n\n**<b>ᴠᴀʟɪᴅɪᴛʏ :</b>** ʟɪғᴇᴛɪᴍᴇ"
+        text = f"User ID:- ```{message.from_user.id}```\nPʟᴀɴ :- {user}\nDᴀʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ :- {humanbytes(limit)}\nTᴏᴅᴀʏ ᴜsᴇᴅ :- {humanbytes(used)}\nRᴇᴍᴀɪɴ:- {humanbytes(remain)}"
     else:
         normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
-        text = f"**User ID :** `{message.from_user.id}` \n**Name :** {message.from_user.mention} \n\n**<b>🏷 ᴘʟᴀɴ :</b>** {user} \n\n<b>✓ ʜɪɢʜ ᴘʀɪᴏʀɪᴛʏ </b>\n<b>✓ ᴜᴘʟᴏᴀᴅ 4GB ғɪʟᴇs </b>\n<b>✓ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ </b>: {humanbytes(limit)} \n<b>✓ ᴛᴏᴅᴀʏ ᴜsᴇᴅ </b>: {humanbytes(used)} \n<b>✓ ʀᴇᴍᴀɪɴ :</b> {humanbytes(remain)} \n<b>✓ ᴛɪᴍᴇᴏᴜᴛ :</b> 0 sᴇᴄᴏɴᴅ \n</b>✓ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇss :</b> ᴜɴʟɪᴍɪᴛᴇᴅ \n<b>✓ ᴛɪᴍᴇ ɢᴀᴘ :</b> Yes \n\n**<b>ʏᴏᴜʀ ᴘʟᴀɴ ᴇɴᴅs ᴏɴ </b>:** {normal_date}"
+        text = f"User ID:- ```{message.from_user.id}```\nPʟᴀɴ :- {user}\nDᴀʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ :- {humanbytes(limit)}\nTᴏᴅᴀʏ ᴜsᴇᴅ :- {humanbytes(used)}\nRᴇᴍᴀɪɴ:- {humanbytes(remain)}\n\nYᴏᴜʀ ᴘʟᴀɴ ᴇɴᴅs ᴏɴ :- {normal_date}"
 
     if user == "Free":
-        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("<b>💳 ᴜᴘɢʀᴀᴅᴇ</b>", callback_data="upgrade"), InlineKeyboardButton("✘ ᴄᴀɴᴄᴇʟ", callback_data="cancel")]]))
+        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💳 Uᴘɢʀᴀᴅᴇ", callback_data="upgrade"), InlineKeyboardButton("✘ ᴄᴀɴᴄᴇʟ", callback_data="cancel")]]))
     else:
-        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("<b>✘ ᴄᴀɴᴄᴇʟ ✘</b>", callback_data="cancel")]]))
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+        await message.reply(text, quote=True)
