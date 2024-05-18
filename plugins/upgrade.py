3725b67fcb3c7510c75baa0c734d43e3e43d76db
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-18 15:40:44 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -3,63 +3,38 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """**ғʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
-	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 𝟸ɢʙ
-	ᴘʀɪᴄᴇ 𝟶
+	text = """**๏ Fʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
+
+        ☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2.0 GB
+        ☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2
+        ☞ ᴛɪᴍᴇᴏᴜᴛ: 1 ᴍɪɴᴜᴛᴇꜱ
+        ☞ ᴛɪᴍᴇ ɢᴀᴘ: ʏᴇꜱ
 	
-	**🔥 Bᴀsɪᴄ** 
-	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ  ʟɪᴍɪᴛ 20GB
-	ᴘʀɪᴄᴇ ʀs 49  ɪɴᴅɪᴀ /🌎 0.59$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	**😎 Sᴛᴀɴᴅᴀʀᴅ**
-	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 50GB
-	ᴘʀɪᴄᴇ ʀs 99  ɪɴᴅɪᴀ /🌎 1.19$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	**👑 Uʟᴛɪᴍᴀᴛᴇ**
-	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 100GB
-	ᴘʀɪᴄᴇ ʀs 179  ɪɴᴅɪᴀ /🌎 2.16$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	
-	ᴘᴀʏ ᴜsɪɴɢ ᴜᴘɪ ɪ'ᴅ `anime-legend@axl`
-	
-	<b>ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛs ᴏғ</b> 
-        <b>ᴘᴀʏᴍᴇɴᴛ ᴛᴏ ᴀᴅᴍɪɴ @Devil_Eyes_ZX</b>"""
+	**▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼**"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Aᴅᴍɪɴ",url = "https://t.me/Devil_Eyes_ZX")], 
-        			[InlineKeyboardButton("Pʜᴏɴᴇ ᴘᴀʏ",url = "https://telegra.ph/file/7875724e5ead1d6ded2b4.jpg"),
-        			InlineKeyboardButton("Gᴏᴏɢʟᴇ ᴘᴀʏ",url = "https://telegra.ph/file/e08ee420fd2c1c27586cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        		       [InlineKeyboardButton("ғʀᴇᴇ", callback_data='free'),
+                                InlineKeyboardButton("sɪʟᴠᴇʀ", callback_data='silver')],
+                               [InlineKeyboardButton("ɢᴏʟᴅᴇɴ", callback_data='golden'),
+			        InlineKeyboardButton("ᴅɪᴀᴍᴏɴᴅ", callback_data='diamond')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**ғʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
-	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 2GB
-	ᴘʀɪᴄᴇ 0
-	
-	**🔥 Bᴀsɪᴄ** 
-	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ  ʟɪᴍɪᴛ 20GB
-	ᴘʀɪᴄᴇ ʀs 49  ɪɴᴅɪᴀ /🌎 0.59$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	**😎 Sᴛᴀɴᴅᴀʀᴅ**
-	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 50GB
-	ᴘʀɪᴄᴇ ʀs 99  ɪɴᴅɪᴀ /🌎 1.19$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	**👑 Uʟᴛɪᴍᴀᴛᴇ**
-	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 100GB
-	ᴘʀɪᴄᴇ ʀs 179  ɪɴᴅɪᴀ /🌎 2.16$  ᴘᴇʀ ᴍᴏɴᴛʜ
-	
-	
-	ᴘᴀʏ ᴜsɪɴɢ ᴜᴘɪ ɪ'ᴅ `anime-legend@axl`
-	
-	<b>ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛs ᴏғ</b> 
-        <b>ᴘᴀʏᴍᴇɴᴛ ᴛᴏ ᴀᴅᴍɪɴ @Devil_Eyes_ZX</b>"""
-	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/Devil_Eyes_ZX")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://telegra.ph/file/7875724e5ead1d6ded2b4.jpg"),
-        			InlineKeyboardButton("Google Pay",url = "https://telegra.ph/file/e08ee420fd2c1c27586cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await message.reply_text(text = text,reply_markup = keybord)
+	text = """**๏ Fʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
 
+        ☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2.0 GB
+        ☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2
+        ☞ ᴛɪᴍᴇᴏᴜᴛ: 1 ᴍɪɴᴜᴛᴇꜱ
+        ☞ ᴛɪᴍᴇ ɢᴀᴘ: ʏᴇꜱ
+	
+	**▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼**"""
+	keybord = InlineKeyboardMarkup([[ 
+        		       [InlineKeyboardButton("ғʀᴇᴇ", callback_data='free'),
+                                InlineKeyboardButton("sɪʟᴠᴇʀ", callback_data='silver')],
+                               [InlineKeyboardButton("ɢᴏʟᴅᴇɴ", callback_data='golden'),
+			        InlineKeyboardButton("ᴅɪᴀᴍᴏɴᴅ", callback_data='diamond')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
 
 
 
