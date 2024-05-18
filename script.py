0d736b828a9daaa6498b17490c4eb155dd746953
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-18 19:09:42 +0530

    Update script.py

--- script.py
+++ script.py
@@ -45,48 +45,7 @@ If You Like My Bots & Projects, You Can Donate Me Any Amount From 10 Rs Upto You
 <b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
 <b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
 
-     PREMIUM_TXT= """<b>Free Plan Users</b>
 
-<b>☞ Daily Upload: 2.0 GB</b>
-<b>☞ Parallel Process: 2</b>
-<b>☞ Timeout: 1  Minutes</b>
-<b>☞ Time Gap: Yes</b>
-	
-<b>▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼</b>"""
 
-     SILVER_TXT="""<b>🥈 Silver Plan</b>
 
-<b>☞ Upload 4GB Files: True</b>
-<b>☞ Daily Upload: 20.GB</b>
-<b>☞ Parallel Process: 2</b>
-<b>☞ Time Gap: No Time Gap</b>
-
-<b>💰 <b>Plan Price</>
-
-<b>☞ Price: ₹49</b>
-<b>☞ Validity: 30Days</b>"""
-
-     GOLDEN_TXT="""<b>🎖 Golden Plan</b>
-
-<b>☞ Upload 4GB Files: True</b>
-<b>☞ Daily Upload: 50.GB</b>
-<b>☞ Parallel Process: 3</b>
-<b>☞ Time Gap: No Time Gap</b>
-
-<b>💰 Plan Price</b>
-
-<b>☞ Price: ₹99</b>
-<b>☞ Validity: 30Days</b>"""
-
-     DIAMOND_TXT= """<b>💎 Diamond Plan</b>
-
-<b>☞ Upload 4GB Files: True</b>
-<b>☞ Daily Upload: 100.GB</b>
-<b>☞ Parallel Process: 5</b>
-<b>☞ Time Gap: No Time Gap</b>
-
-<b>💰 Plan Price</b>
-
-<b>☞ Price: ₹159</b>
-<b>☞ Validity: 30Days</b>"""
 
