a2a63637965e6ce071c86e09b1d9f0153133216b
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-18 17:53:38 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -3,111 +3,71 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """**๏ Fʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
-
-        ☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2.0 GB
-        ☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2
-        ☞ ᴛɪᴍᴇᴏᴜᴛ: 1 ᴍɪɴᴜᴛᴇꜱ
-        ☞ ᴛɪᴍᴇ ɢᴀᴘ: ʏᴇꜱ
+	text = """**ғʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
+	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 𝟸ɢʙ
+	ᴘʀɪᴄᴇ 𝟶
 	
-	**▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼**"""
+	**🔥 Bᴀsɪᴄ** 
+	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ  ʟɪᴍɪᴛ 20GB
+	ᴘʀɪᴄᴇ ʀs 49  ɪɴᴅɪᴀ /🌎 0.59$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	**😎 Sᴛᴀɴᴅᴀʀᴅ**
+	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 50GB
+	ᴘʀɪᴄᴇ ʀs 99  ɪɴᴅɪᴀ /🌎 1.19$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	**👑 Uʟᴛɪᴍᴀᴛᴇ**
+	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 100GB
+	ᴘʀɪᴄᴇ ʀs 159  ɪɴᴅɪᴀ /🌎 2.16$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	
+	ᴘᴀʏ ᴜsɪɴɢ ᴜᴘɪ ɪ'ᴅ `anime-legend@axl`
+	
+	<b>ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛs ᴏғ</b> 
+        <b>ᴘᴀʏᴍᴇɴᴛ ᴛᴏ ᴀᴅᴍɪɴ @Devil_Eyes_ZBot</b>"""
 	keybord = InlineKeyboardMarkup([[ 
-        		       [InlineKeyboardButton("ғʀᴇᴇ", callback_data='free'),
-                                InlineKeyboardButton("sɪʟᴠᴇʀ", callback_data='silver')],
-                               [InlineKeyboardButton("ɢᴏʟᴅᴇɴ", callback_data='golden'),
-			        InlineKeyboardButton("ᴅɪᴀᴍᴏɴᴅ", callback_data='diamond')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Aᴅᴍɪɴ",url = "https://t.me/Devil_Eyes_ZBot")], 
+        			[InlineKeyboardButton("Pʜᴏɴᴇ ᴘᴀʏ",url = "https://telegra.ph/file/7875724e5ead1d6ded2b4.jpg"),
+        			InlineKeyboardButton("Gᴏᴏɢʟᴇ ᴘᴀʏ",url = "https://telegra.ph/file/e08ee420fd2c1c27586cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**๏ Fʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
-
-        ☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2.0 GB
-        ☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2
-        ☞ ᴛɪᴍᴇᴏᴜᴛ: 1 ᴍɪɴᴜᴛᴇꜱ
-        ☞ ᴛɪᴍᴇ ɢᴀᴘ: ʏᴇꜱ
+	text = """**ғʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀ**
+	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 2GB
+	ᴘʀɪᴄᴇ 0
 	
-	**▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼**"""
+	**🔥 Bᴀsɪᴄ** 
+	ᴅᴀɪʟʏ  ᴜᴘʟᴏᴀᴅ  ʟɪᴍɪᴛ 20GB
+	ᴘʀɪᴄᴇ ʀs 49  ɪɴᴅɪᴀ /🌎 0.59$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	**😎 Sᴛᴀɴᴅᴀʀᴅ**
+	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 50GB
+	ᴘʀɪᴄᴇ ʀs 99  ɪɴᴅɪᴀ /🌎 1.19$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	**👑 Uʟᴛɪᴍᴀᴛᴇ**
+	ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 100GB
+	ᴘʀɪᴄᴇ ʀs 159  ɪɴᴅɪᴀ /🌎 2.16$  ᴘᴇʀ ᴍᴏɴᴛʜ
+	
+	
+	ᴘᴀʏ ᴜsɪɴɢ ᴜᴘɪ ɪ'ᴅ `anime-legend@axl`
+	
+	<b>ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛs ᴏғ</b> 
+        <b>ᴘᴀʏᴍᴇɴᴛ ᴛᴏ ᴀᴅᴍɪɴ @Devil_Eyes_ZBot</b>"""
 	keybord = InlineKeyboardMarkup([[ 
-        		       [InlineKeyboardButton("ғʀᴇᴇ", callback_data='free'),
-                                InlineKeyboardButton("sɪʟᴠᴇʀ", callback_data='silver')],
-                               [InlineKeyboardButton("ɢᴏʟᴅᴇɴ", callback_data='golden'),
-			        InlineKeyboardButton("ᴅɪᴀᴍᴏɴᴅ", callback_data='diamond')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
+        			InlineKeyboardButton("Admin",url = "https://t.me/Devil_Eyes_ZX")], 
+        			[InlineKeyboardButton("Phone Pay",url = "https://telegra.ph/file/7875724e5ead1d6ded2b4.jpg"),
+        			InlineKeyboardButton("Google Pay",url = "https://telegra.ph/file/e08ee420fd2c1c27586cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
 
 
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-from pyrogram import Client , filters
 
-@Client.on_callback_query(filters.regex('free'))
-async def upgrade(bot,update):
-	text = """**❤️‍🔥 <u>sɪʟᴠᴇʀ ᴘʟᴀɴ</u>**
 
-        <b>☞ ᴜᴘʟᴏᴀᴅ 2ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-        <b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2ɢʙ</b>
-        <b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2</b>
-        <b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
 
-        **💰 <u>ᴘʟᴀɴ ᴘʀɪᴄᴇ</u>**
 
-        <b>☞ ᴘʀɪᴄᴇ: 0</b>
-        <b>☞ ᴠᴀʟɪᴅɪᴛʏ: ʟɪғᴇᴛɪᴍᴇ</b>"""
-	keybord = InlineKeyboardMarkup([[ 
-        			[InlineKeyboardButton("《 Bᴀᴄᴋ", callback_data='upgrade'),
-        			InlineKeyboardButton("Bᴜʏ",url = " https://t.me/Devil_Eyes_ZBot")] ]]) 
-	await update.message.edit(text = text,reply_markup = keybord)
 
-@Client.on_callback_query(filters.regex('silver'))
-async def upgrade(bot,update):
-	text = """**🥈 <u>sɪʟᴠᴇʀ ᴘʟᴀɴ</u>**
 
-        <b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-        <b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 20.0ɢʙ</b>
-        <b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2</b>
-        <b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
-
-        **💰 <u>ᴘʟᴀɴ ᴘʀɪᴄᴇ</u>**
-
-        <b>☞ ᴘʀɪᴄᴇ: ₹49</b>
-        <b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
-	keybord = InlineKeyboardMarkup([[ 
-        			[InlineKeyboardButton("《 Bᴀᴄᴋ", callback_data='upgrade'),
-        			InlineKeyboardButton("Bᴜʏ",url = " https://t.me/Devil_Eyes_ZBot")] ]]) 
-	await update.message.edit(text = text,reply_markup = keybord)
-
-@Client.on_callback_query(filters.regex('golden'))
-async def upgrade(bot,update):
-	text = """**🎖 <u>Gᴏʟᴅᴇɴ ᴘʟᴀɴ</u>**
-
-        <b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-        <b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 50.0ɢʙ</b>
-        <b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 3</b>
-        <b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
-
-        **💰 <u>ᴘʟᴀɴ ᴘʀɪᴄᴇ</u>**
-
-        <b>☞ ᴘʀɪᴄᴇ: ₹99</b>
-        <b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
-	keybord = InlineKeyboardMarkup([[ 
-        			[InlineKeyboardButton("《 Bᴀᴄᴋ", callback_data='upgrade'),
-        			InlineKeyboardButton("Bᴜʏ",url = " https://t.me/Devil_Eyes_ZBot")] ]]) 
-	await update.message.edit(text = text,reply_markup = keybord)
-
- @Client.on_callback_query(filters.regex('diamond'))
-async def upgrade(bot,update):
-	text = """**💎 <u>Dɪᴀᴍᴏɴᴅ ᴘʟᴀɴ</u>**
-
-        <b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-        <b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 100.0ɢʙ</b>
-        <b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 5</b>
-        <b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
-
-        **💰 <u>ᴘʟᴀɴ ᴘʀɪᴄᴇ</u>**
-
-        <b>☞ ᴘʀɪᴄᴇ: ₹159</b>
-        <b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
-	keybord = InlineKeyboardMarkup([[ 
-        			[InlineKeyboardButton("《 Bᴀᴄᴋ", callback_data='upgrade'),
-        			InlineKeyboardButton("Bᴜʏ",url = " https://t.me/Devil_Eyes_ZBot")] ]]) 
-	await update.message.edit(text = text,reply_markup = keybord)
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Developer @JishuDeveloper
