4a61ba6d0bf62a6fa4e8674ff5f32f2edd6d873b
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-19 08:52:57 +0530

    Update scriptz.py

--- scriptz.py
+++ scriptz.py
@@ -1,45 +1,45 @@
 class scriptz(object):
-         PREMIUM_TXT= """<b>๏ Fʀᴇᴇ ᴘʟᴀɴ ᴜsᴇʀs</b>
+    PREMIUM_TXT= """<b>Free Plan Users</b>
 
-<b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 2.0 GB</b>
-<b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2</b>
-<b>☞ ᴛɪᴍᴇᴏᴜᴛ: 1 ᴍɪɴᴜᴛᴇꜱ</b>
-<b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ʏᴇꜱ</b>
-	
-<b>▼ Uᴘɢʀᴀᴅᴇ ᴘʟᴀɴ ▼</b>"""
+<b>☞ Daily Upload: 2.0 GB</b>
+<b>☞ Parallel Process: 2</b>
+<b>☞ Timeout: 1 Minutes</b>
+<b>☞ Time Gap: Yes</b>
+ 
+<b>▼ Upgrade Plan ▼</b>"""
 
-     SILVER_TXT="""<b>🥈 Silver Plan</b>
+    SILVER_TXT= """<b>🥈 Silver Plan</b>
 
-<b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-<b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 20.0ɢʙ</b>
-<b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 2</b>
-<b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
+<b>☞ Upload 4GB Files: True</b>
+<b>☞ Daily Upload: 20GB</b>
+<b>☞ Parallel Process: 2</b>
+<b>☞ Time Gap: No Time Gap</b>
 
-<b>💰 <b>ᴘʟᴀɴ ᴘʀɪᴄᴇ</>
+<b>💰 <b>Plan Price</>
 
-<b>☞ ᴘʀɪᴄᴇ: ₹49</b>
-<b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
+<b>☞ Price: ₹49</b>
+<b>☞ Validity: 30days</b>"""
 
-     GOLDEN_TXT="""<b>🎖 Golden Plan</b>
+    GOLDEN_TXT= """<b>🎖 Golden Plan</b>
 
-<b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-<b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 50.0ɢʙ</b>
-<b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 3</b>
-<b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
+<b>☞ Upload 4GB Files: True</b>
+<b>☞ Daily Upload: 50GB</b>
+<b>☞ Parallel Process: 3</b>
+<b>☞ Time Gap: No Time Gap</b>
 
-<b>💰 ᴘʟᴀɴ ᴘʀɪᴄᴇ</b>
+<b>💰 Plan Price</b>
 
-<b>☞ ᴘʀɪᴄᴇ: ₹99</b>
-<b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
+<b>☞ Price: ₹99</b>
+<b>☞ Validity: 30days</b>"""
 
-     DIAMOND_TXT= """<b>💎 Diamond Plan</b>
+    DIAMOND_TXT= """<b>💎 Diamond Plan</b>
 
-<b>☞ ᴜᴘʟᴏᴀᴅ 4ɢʙ ꜰɪʟᴇꜱ: ᴛʀᴜᴇ</b>
-<b>☞ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ: 100.0ɢʙ</b>
-<b>☞ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇꜱꜱ: 5</b>
-<b>☞ ᴛɪᴍᴇ ɢᴀᴘ: ɴᴏ ᴛɪᴍᴇ ɢᴀᴘ</b>
+<b>☞ Upload 4GB Files: True</b>
+<b>☞ Daily Upload: 100GB</b>
+<b>☞ Parallel Process: 5</b>
+<b>☞ Time Gap: No Time Gap</b>
 
-<b>💰 ᴘʟᴀɴ ᴘʀɪᴄᴇ</b>
+<b>💰 Plan Price</b>
 
-<b>☞ ᴘʀɪᴄᴇ: ₹159</b>
-<b>☞ ᴠᴀʟɪᴅɪᴛʏ: 𝟹𝟶ᴅᴀʏs</b>"""
+<b>☞ Price: ₹159</b>
+<b>☞ Validity: 30days</b>"""
