31a9d354020789e76ad3a4a1947a7e294108186b
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-19 09:39:35 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -34,18 +34,18 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """<b>Free Plan Users</b>
+	text = """<b>๏ Free Plan Users</b>
 
-        <b>☞ Daily Upload: 2.0 GB</b>
-        <b>☞ Parallel Process: 2</b>
-        <b>☞ Timeout: 1 Minutes</b>
-        <b>☞ Time Gap: Yes</b>
-	
-        <b>▼ Upgrade Plan ▼</b>"""
+<b>☞ Daily Upload: 2.0 GB</b>
+<b>☞ Parallel Process: 2</b>
+<b>☞ Timeout: 1 Minutes</b>
+<b>☞ Time Gap: Yes</b>
+
+<b>▼ Upgrade Plan ▼</b>"""
 	keybord = InlineKeyboardMarkup([
                                [InlineKeyboardButton('Sɪʟᴠᴇʀ', callback_data='silver'),
                                InlineKeyboardButton('Gᴏʟᴅᴇɴ', callback_data='golden')],
-                               [InlineKeyboardButton('Dᴜʏ', url = "https://t.me/Devil_Eyes_ZBot"),
+                               [InlineKeyboardButton('Bᴜʏ', url = "https://t.me/Devil_Eyes_ZBot"),
                                InlineKeyboardButton('Dɪᴀᴍᴏɴᴅ', callback_data='diamond')] 
 	                       ]) 
 	await message.reply_text(text = text,reply_markup = keybord)
